a2a82ddf358b5e8069c2df0e83989608a10f3810
Roshan54321 <roshanneupane54321@gmail.com>
2025-02-06 13:07:42 +0545

    fixed app.js

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,14 @@ import {
   useNavigationType,
   useLocation,
 } from "react-router-dom";
+import Badge1 from "./components/Badge1";
+import Button1 from "./components/Button1";
+import Calendar1 from "./components/Calendar1";
+import Table1 from "./components/Table1";
+import Skeleton1 from "./components/Skeleton1";
 import DropdownMenu1 from "./components/DropdownMenu1";
+import DataTable1 from "./components/DataTable1";
+import NavigationMenu1 from "./components/NavigationMenu1";
 
 function App() {
   const action = useNavigationType();
@@ -45,7 +52,14 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<DropdownMenu1 />} />
+      <Route path="/" element={<Badge1 />} />
+      <Route path="/button" element={<Button1 variant="destructive" />} />
+      <Route path="/calendar" element={<Calendar1 />} />
+      <Route path="/table" element={<Table1 />} />
+      <Route path="/skeleton" element={<Skeleton1 />} />
+      <Route path="/dropdownmenu" element={<DropdownMenu1 />} />
+      <Route path="/datatable" element={<DataTable1 />} />
+      <Route path="/navigationmenu" element={<NavigationMenu1 />} />
     </Routes>
   );
 }
